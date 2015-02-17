0fe6559812521684ccc699dda9fd785eb371d2d4
Maxim Dementyev <max@orofarne.ru>
2015-02-17 23:09:16 +0300

    tmp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
+var sass = require('gulp-sass');
 
 gulp.task('bower', function() {
 	return bower()
 		.pipe(gulp.dest('lib/'))
+});
+
+gulp.task('sass', function () {
+	// main.css
+	gulp.src('./scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./css/'));
 });
