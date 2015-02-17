31e07b17096b332fb9b9ba9d449e8185fc814f02
Maxim Dementyev <max@orofarne.ru>
2015-02-18 00:03:02 +0300

    css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,32 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
 var sass = require('gulp-sass');
+var flatten = require('gulp-flatten');
+var rename = require("gulp-rename");
 
 gulp.task('bower', function() {
 	return bower()
-		.pipe(gulp.dest('lib/'))
+});
+
+gulp.task('lib', ['bower'], function() {
+	// CSS -> SCSS
+	gulp.src('./bower_components/*/dist/*.css')
+		.pipe(flatten())
+		.pipe(rename(function (path) {
+			path.basename = '_' + path.basename;
+			path.extname = path.extname.replace('css', 'scss');
+		}))
+		.pipe(gulp.dest('./lib/scss/'));
+
+	// JS
+	gulp.src(['./bower_components/*/dist/*.js', '!**/*-src.js'])
+		.pipe(flatten())
+		.pipe(gulp.dest('./lib/js/'));
 });
 
 gulp.task('sass', function () {
 	// main.css
 	gulp.src('./scss/*.scss')
-		.pipe(sass())
+		.pipe(sass({ includePaths: ['./lib/scss'], errLogToConsole: true }))
 		.pipe(gulp.dest('./css/'));
 });
